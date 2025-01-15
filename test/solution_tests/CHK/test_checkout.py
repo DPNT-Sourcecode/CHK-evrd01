f8c99969943c89dc0b49028a74ab87fa8c9ced07
sbx_user1051 <sbx_user1051@169.254.72.150>
2025-01-15 13:14:17 +0000

    Wed Jan 15 13:14:17 UTC 2025

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,8 +1,22 @@
-
 from solutions.CHK import checkout_solution
 
 class TestCheckout:
-    def test_checkout_1(self):
-        items = "ABCD"
+    def test_checkout_A(self):
+        items = "A"
         price = checkout_solution.checkout(items)
-        assert price == 115
+        assert price == 50
+
+    def test_checkout_B(self):
+        items = "B"
+        price = checkout_solution.checkout(items)
+        assert price == 30
+
+    def test_checkout_C(self):
+        items = "C"
+        price = checkout_solution.checkout(items)
+        assert price == 20
+
+    def test_checkout_D(self):
+        items = "D"
+        price = checkout_solution.checkout(items)
+        assert price == 15
