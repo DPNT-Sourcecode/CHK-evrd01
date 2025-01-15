4e81136ff4d173857109b44cd225c7ec356dd73f
sbx_user1051 <sbx_user1051@169.254.72.150>
2025-01-15 13:05:16 +0000

    Wed Jan 15 13:05:16 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,24 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
+
 def checkout(skus):
-    raise NotImplementedError()
+    prices = {
+        "A": 50,
+        "B": 30,
+        "C": 25,
+        "D": 15
+    }
+
+    purchases = {
+        "A": 0,
+        "B": 0,
+        "C": 0,
+        "D": 0
+    }
+
+    for char in skus:
+        if char not in prices:
+            raise Exception("Invalid SKU")
+        
+
+
