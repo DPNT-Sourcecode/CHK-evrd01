dc9857305f7585603693ae15a14f7e7aa92b7542
sbx_user1051 <sbx_user1051@169.254.72.150>
2025-01-15 13:42:31 +0000

    Wed Jan 15 13:42:31 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,6 +3,8 @@
 
 def checkout(skus: str) -> int:
     prices = {
+        "5A": 200,
+        "3A": 130,
         "A": 50,
         "B": 30,
         "C": 20,
@@ -27,24 +29,35 @@ def checkout(skus: str) -> int:
         
         purchases[char] += 1
         
-        if char == "A":
-            if purchases["A"] % 5 == 0 and purchases["A"] != 0:
-                continue
-            elif purchases["A"] % 3 == 0 and purchases["A"] != 0:
-                total += 30
-                continue
+        # if char == "A":
+            # if purchases["A"] % 5 == 0 and purchases["A"] != 0:
+                # continue
+            # elif purchases["A"] % 3 == 0 and purchases["A"] != 0:
+                # total += 30
+                # continue
 
-        if char == "B" and purchases["B"] % 2 == 0 and purchases["B"] != 0:
-            total += 15
-            continue
+        # if char == "B" and purchases["B"] % 2 == 0 and purchases["B"] != 0:
+            # total += 15
+            # continue
 
-        if char == "E" and purchases["E"] % 2 == 0 and purchases["E"] != 0:
-            purchases["B"] += 1
-            if purchases["B"] %2 == 0:
-                total -= 15
+        # if char == "E" and purchases["E"] % 2 == 0 and purchases["E"] != 0:
+            # purchases["B"] += 1
+            # if purchases["B"] %2 == 0:
+                # total -= 15
+
+        for key, value in dict.items():
+            subtotal = 0
+            if key == "A":
+                fives = value // 5
+                threes = (value % 5) // 3
+                ones = (value % 5) % 3
+                subtotal += prices["5A"] * fives
+                subtotal += prices["3A"] * fives
+                subtotal += prices["A"] * fives
 
         total += prices[char]
     
     return total
+
 
 
